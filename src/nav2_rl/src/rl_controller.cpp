da377bec073d0e07a45159412c41d70e8461ea62
Florian Kunz <s220670@th-ab.de>
2025-12-23 15:23:11 +0100

    Parameter_File::marked tolerance fields, added a layer in global costmap, less failure tolerance; RL_Controller:: some adjustments for calling declared parameters again

--- src/nav2_rl/src/rl_controller.cpp
+++ src/nav2_rl/src/rl_controller.cpp
@@ -16,6 +16,26 @@ void RLController::configure(
   name_ = name;
   tf_ = tf;
   costmap_ros_ = costmap_ros;
+  
+  auto declare_if_not_declared =
+  [this](const std::string & full_name, const auto & default_value) {
+    using T = std::decay_t<decltype(default_value)>;
+    if (!node_->has_parameter(full_name)) {
+      node_->declare_parameter<T>(full_name, default_value);
+    }
+    return node_->get_parameter(full_name).get_value<T>();
+  };
+
+agent_service_      = declare_if_not_declared(name_ + ".agent_service", std::string("/agents/dqn/get_action"));
+timeout_ms_         = declare_if_not_declared(name_ + ".timeout_ms", 150);
+max_lin_            = declare_if_not_declared(name_ + ".max_lin_vel", 0.22);
+max_ang_            = declare_if_not_declared(name_ + ".max_ang_vel", 1.5);
+lookahead_dist_     = declare_if_not_declared(name_ + ".lookahead_dist", 1.0);
+reach_radius_       = declare_if_not_declared(name_ + ".reach_radius", 0.20);
+advance_hysteresis_ = declare_if_not_declared(name_ + ".advance_hysteresis", 0.05);
+
+  
+  /*
 
   agent_service_     = node_->declare_parameter<std::string>(name_ + ".agent_service", "/agents/dqn/get_action");
   timeout_ms_        = node_->declare_parameter<int>(name_ + ".timeout_ms", 150); //so lange wird auf eine Antwort des Servers gewartet
@@ -24,7 +44,7 @@ void RLController::configure(
   lookahead_dist_    = node_->declare_parameter<double>(name_ + ".lookahead_dist", 1.0); //wie weit soll der lokale zielpunkt entlang des globalen pfades weg sein
   reach_radius_      = node_->declare_parameter<double>(name_ + ".reach_radius", 0.20); //Radius um Ziel, in dem es als erreicht gilt und neues lokales Goal gepublisht wird
   advance_hysteresis_= node_->declare_parameter<double>(name_ + ".advance_hysteresis", 0.05); //Mindestverschiebung, um ein neues lokales Ziel zu publizieren 
-  //(damit nicht bei kleinsten Änderungen neu gesendet wird). Ist das sinnvoll ? Dann wird ja wenn das Ziel weniger als das weg liegt, nichts mehr gepublisht???
+  //(damit nicht bei kleinsten Änderungen neu gesendet wird). Ist das sinnvoll ? Dann wird ja wenn das Ziel weniger als das weg liegt, nichts mehr gepublisht??? */
 
   // Publisher für lokales Ziel
   rclcpp::QoS qos(1); qos.transient_local().reliable();
